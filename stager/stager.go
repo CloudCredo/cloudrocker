df83564e2a86e5d4ca06d481e2a557146032f088
Colin Humphreys <colin@hatofmonkeys.com>
2014-07-28 15:00:08 +0100

    Converted stager to using SubDirs from utils

--- stager/stager.go
+++ stager/stager.go
@@ -4,9 +4,10 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/hatofmonkeys/cloudfocker/utils"
 
 	"github.com/cloudfoundry-incubator/linux-circus/buildpackrunner"
 	"github.com/cloudfoundry-incubator/runtime-schema/models"
@@ -23,33 +24,28 @@ func RunBuildpack(writer io.Writer, runner BuildpackRunner) error {
 
 func NewBuildpackRunner(buildpackDir string) *buildpackrunner.Runner {
 	prepareMd5BuildpacksDir(buildpackDir, "/tmp/buildpacks")
-	config := models.NewCircusTailorConfig(subDirs(buildpackDir))
+	var err error
+	dirs := []string{}
+	if dirs, err = utils.SubDirs(buildpackDir); err != nil {
+		log.Fatalf(" %s", err)
+	}
+	config := models.NewCircusTailorConfig(dirs)
 	return buildpackrunner.New(&config)
 }
 
 func prepareMd5BuildpacksDir(src string, dst string) {
 	os.MkdirAll(src, 0755)
 	os.MkdirAll(dst, 0755)
-	dirs := subDirs(src)
+	var err error
+	dirs := []string{}
+	if dirs, err = utils.SubDirs(src); err != nil {
+		log.Fatalf(" %s", err)
+	}
 	for _, dir := range dirs {
 		if err := os.Symlink(src+"/"+dir, dst+"/"+md5sum(dir)); err != nil {
 			log.Fatalf(" %s", err)
 		}
 	}
-}
-
-func subDirs(dir string) (dirs []string) {
-	var contents []os.FileInfo
-	var err error
-	if contents, err = ioutil.ReadDir(dir); err != nil {
-		log.Fatalf(" %s", err)
-	}
-	for _, file := range contents {
-		if file.IsDir() {
-			dirs = append(dirs, file.Name())
-		}
-	}
-	return
 }
 
 func md5sum(src string) string {
