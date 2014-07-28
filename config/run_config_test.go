bb0446e4037c92398d0b4c397bcfb7dff4886e18
Colin Humphreys <colin@hatofmonkeys.com>
2014-07-28 19:18:03 +0100

    clarify build/stage config in RunConfig test

--- config/run_config_test.go
+++ config/run_config_test.go
@@ -10,12 +10,12 @@ import (
 var _ = Describe("RunConfig", func() {
 	Describe("Generating a RunConfig for staging", func() {
 		It("should return a valid RunConfig with the correct staging information", func() {
-			buildConfig := config.NewStageRunConfig("/home/testuser/testapp")
-			Expect(buildConfig.ContainerName).To(Equal("cloudfocker-staging"))
-			Expect(buildConfig.ImageTag).To(Equal("cloudfocker-base:latest"))
-			Expect(len(buildConfig.Mounts)).To(Equal(6))
-			Expect(buildConfig.Mounts["/home/testuser/testapp"]).To(Equal("/app"))
-			Expect(buildConfig.RunCommand).To(Equal([]string{"/focker/fock", "stage"}))
+			stageConfig := config.NewStageRunConfig("/home/testuser/testapp")
+			Expect(stageConfig.ContainerName).To(Equal("cloudfocker-staging"))
+			Expect(stageConfig.ImageTag).To(Equal("cloudfocker-base:latest"))
+			Expect(len(stageConfig.Mounts)).To(Equal(6))
+			Expect(stageConfig.Mounts["/home/testuser/testapp"]).To(Equal("/app"))
+			Expect(stageConfig.RunCommand).To(Equal([]string{"/focker/fock", "stage"}))
 		})
 	})
 })
