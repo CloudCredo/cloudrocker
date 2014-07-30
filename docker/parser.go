5bdba9c86fa68a4b22e36964c83fdc71c99860ac
Colin Humphreys <colin@hatofmonkeys.com>
2014-07-30 07:43:29 +0100

    Moved parsed argument sorting to parsing functions

--- docker/parser.go
+++ docker/parser.go
@@ -10,18 +10,12 @@ import (
 )
 
 func ParseRunCommand(config *config.RunConfig) (runCmd []string) {
-	mounts := parseMounts(config.Mounts)
-	sort.Strings(mounts)
-	runCmd = append(runCmd, mounts...)
+	runCmd = append(runCmd, parseMounts(config.Mounts)...)
 	runCmd = append(runCmd, userString())
 	runCmd = append(runCmd, parseContainerName(config.ContainerName)...)
 	runCmd = append(runCmd, parseDaemon(config.Daemon)...)
-	envVars := parseEnvVars(config.EnvVars)
-	sort.Strings(envVars)
-	runCmd = append(runCmd, envVars...)
-	publishedPorts := parsePublishedPorts(config.PublishedPorts)
-	sort.Strings(publishedPorts)
-	runCmd = append(runCmd, publishedPorts...)
+	runCmd = append(runCmd, parseEnvVars(config.EnvVars)...)
+	runCmd = append(runCmd, parsePublishedPorts(config.PublishedPorts)...)
 	runCmd = append(runCmd, parseImageTag(config.ImageTag)...)
 	runCmd = append(runCmd, parseCommand(config.Command)...)
 	return
@@ -32,6 +26,7 @@ func parseMounts(mounts map[string]string) (parsedMounts []string) {
 		parsedMounts = append(parsedMounts,
 			"--volume="+src+":"+dst)
 	}
+	sort.Strings(parsedMounts)
 	return
 }
 
@@ -68,14 +63,16 @@ func parseEnvVars(envVars map[string]string) (parsedEnvVars []string) {
 		parsedEnvVars = append(parsedEnvVars,
 			"--env=\""+key+"="+val+"\"")
 	}
+	sort.Strings(parsedEnvVars)
 	return
 }
 
-func parsePublishedPorts(publishedPorts map[int]int) (parsedEnvVars []string) {
+func parsePublishedPorts(publishedPorts map[int]int) (parsedPublishedPorts []string) {
 	for host, cont := range publishedPorts {
-		parsedEnvVars = append(parsedEnvVars,
+		parsedPublishedPorts = append(parsedPublishedPorts,
 			"--publish="+strconv.Itoa(host)+":"+strconv.Itoa(cont))
 	}
+	sort.Strings(parsedPublishedPorts)
 	return
 }
 
